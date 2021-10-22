5c207dcbdece232dd837ed007616d2b8ea9b15b3
MohanaPriyaSomu <91050383+MohanaPriyaSomu@users.noreply.github.com>
2021-10-22 17:42:29 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,11 +1,33 @@
 #include "stats.h"
 
-struct Stats compute_statistics(const float* numberset, int setlength) {
-    struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
-}//comment
+struct Statistics{
+	float average;
+	float min;
+	float max;
+}s;
 
-int emailAlertCallCount = 0;
-int ledAlertCallCount = 0;
+Stats compute_statistics(const float* numberset, int setlength) 
+{
+    int i = 0;
+	int sum = 0;
+	s.average = 0;
+    s.min = s.max = numberset[0];
+    for (i = 0; i<setlength; i++)
+	{
+		sum += numberset[i];
+		if (s.min > numberset[i])
+		{
+			s.min = numberset[i];
+		}
+		if (s.max < numberset[i])
+		{
+			s.max = numberset[i];
+		}
+	}
+	s.average = sum/setlength;
+	if (setlength == 0)
+	{
+		return NAN; 
+	}
+	return s; // TEST_CASE("reports average, minimum and maximum")
+}
