653792b3ab994c82ebd0a6490a31f9842b394445
MohanaPriyaSomu <91050383+MohanaPriyaSomu@users.noreply.github.com>
2021-10-22 17:44:33 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -2,14 +2,15 @@
 
 #include "catch.hpp"
 #include "stats.h"
+#include "alert.h"
 
 #include <stdlib.h>
 #include <math.h>
-
+#include <cmath.h>
 TEST_CASE("reports average, minimum and maximum") {
     float numberset[] = {1.5, 8.9, 3.2, 4.5};
     int setlength = sizeof(numberset) / sizeof(numberset[0]);
-    struct Stats computedStats = compute_statistics(numberset, setlength);
+    Stats computedStats = compute_statistics(numberset, setlength);
     float epsilon = 0.001;
     REQUIRE(abs(computedStats.average - 4.525) < epsilon);
     REQUIRE(abs(computedStats.max - 8.9) < epsilon);
@@ -21,8 +22,12 @@ TEST_CASE("average is NaN for empty array") {
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     
+	
     //Design the REQUIRE statement here.
     //Use https://stackoverflow.com/questions/1923837/how-to-use-nan-and-inf-in-c
+	REQUIRE(isnan(computedStats.min) == FALSE);
+	REQUIRE(isnan(computedStats.max) == FALSE);
+	REQUIRE(isnan(computedStats.average) == FALSE);
 }
 
 TEST_CASE("raises alerts when max is greater than threshold") {
@@ -35,7 +40,7 @@ TEST_CASE("raises alerts when max is greater than threshold") {
     Stats computedStats = compute_statistics(numberset, setlength);
 
     const float maxThreshold = 10.2;
-    check_and_alert(maxThreshold, alerters, computedStats);
+    check_and_alert(maxThreshold, *alerters, computedStats);
 
     // need a way to check if both emailAlerter, ledAlerter were called
     // you can define call-counters along with the functions, as shown below
