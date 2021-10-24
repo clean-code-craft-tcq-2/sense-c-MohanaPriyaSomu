d6ead9ce6a2be6d9e7354c42fd243b8e124c4c2e
MohanaPriyaSomu <91050383+MohanaPriyaSomu@users.noreply.github.com>
2021-10-24 20:10:54 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -6,6 +6,14 @@
 #ifdef NAN
 /* NAN is supported */
 #endif
+
+struct Statistics
+{
+    float average;
+    float min;
+    float max;
+}Stats;
+
 Stats compute_statistics(const float* numberset, int setlength);
 
 #endif
