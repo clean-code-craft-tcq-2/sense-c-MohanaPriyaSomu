6efef6947a4ba2d3b9d42bd73bb7544047d8c2f3
MohanaPriyaSomu <91050383+MohanaPriyaSomu@users.noreply.github.com>
2021-10-24 19:49:46 +0530

    Update alert.c

--- alert.c
+++ alert.c
@@ -1,9 +1,9 @@
-#include <math.h>
 #include "alert.h"
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats compute_statistics)
+
+void check_and_alert(float maxThreshold, alerter_funcptr alerters, Stats compute_statistics)
 {
 	if (compute_statistics.max > maxThreshold)
 	{
@@ -17,8 +17,7 @@ void emailAlerter()
 	emailAlertCallCount = 1;
 }
 
-void ledAlertCallCount()
+void ledAlerter()
 {
 	ledAlertCallCount = 1;
 }
-		
