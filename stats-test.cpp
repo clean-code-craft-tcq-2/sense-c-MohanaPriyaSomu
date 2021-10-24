d944a6c790174d60448d0c1870ac2ac41efc81a5
MohanaPriyaSomu <91050383+MohanaPriyaSomu@users.noreply.github.com>
2021-10-24 19:51:13 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <cmath.h>
+
 TEST_CASE("reports average, minimum and maximum") {
     float numberset[] = {1.5, 8.9, 3.2, 4.5};
     int setlength = sizeof(numberset) / sizeof(numberset[0]);
