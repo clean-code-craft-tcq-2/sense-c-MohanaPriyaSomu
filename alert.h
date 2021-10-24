0c2e58f7b4f9dcabe279f65ffc8b7394cc0eba3c
MohanaPriyaSomu <91050383+MohanaPriyaSomu@users.noreply.github.com>
2021-10-24 21:01:28 +0530

    Update alert.h

--- alert.h
+++ alert.h
@@ -2,6 +2,8 @@
 #define ALERT_H 
 
 #include "stats.h"
+int emailAlertCallCount;
+int ledAlertCallCount;
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats computedStats);
 void emailAlerter();
