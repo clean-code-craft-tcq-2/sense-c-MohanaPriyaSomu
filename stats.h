6975dc1b5c5c253b633b0e26418a68441201018c
MohanaPriyaSomu <91050383+MohanaPriyaSomu@users.noreply.github.com>
2021-10-24 18:41:06 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,9 +1,6 @@
 #include <math.h>
-typedef struct Stats;
-Stats compute_statistics(const float* numberset, int setlength);
 #ifdef NAN
 /* NAN is supported */
 #endif
-#ifdef INFINITY
-/* INFINITY is supported */
-#endif
+typedef struct Stats;
+Stats compute_statistics(const float* numberset, int setlength);
